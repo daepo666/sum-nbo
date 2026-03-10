d52e9507f24958c922fccaa40b3010f52077e453
daepo666 <firenabang2012@icloud.com>
2026-03-10 21:26:53 +0900

    main.cpp error fixedgit add main.cpp!

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main(){
 	int s = sum(10);
 	printf("Mr. gilgil have a nice day\n");
 	char name[] = "류대호";
-	printf("[sub26-2026]sum-test[%s]", name);
+	printf("[sub26-2026]sum-test[%s]\n", name);
 	printf("result=%d\n",s);
 	
 }

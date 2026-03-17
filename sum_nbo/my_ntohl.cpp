099d51198773e54e4d6c94fef2709f6b8ccba4cf
daepo666 <firenabang2012@icloud.com>
2026-03-17 23:41:08 +0900

    + to |

--- sum_nbo/my_ntohl.cpp
+++ sum_nbo/my_ntohl.cpp
@@ -3,7 +3,7 @@
 uint32_t my_ntohl(uint32_t origin){
     uint32_t result = 0;
     for (int i = 0 ; i<4; i++){
-        result += ((origin&(0xff <<i*8))>>(i*8))<<(24-i*8);
+        result |= ((origin&(0xff <<i*8))>>(i*8))<<(24-i*8);
     }
     return result;
 }
